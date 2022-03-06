d19a97e2b793d2bacb8e7f293c4d490cdbcc8244
Louis Jeon <louisjeon@admins-MacBook-Pro.local>
2022-03-06 20:27:08 +0900

    test

--- client/src/api.ts
+++ client/src/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "https://play-picasso.herokuapp.com/api",
+  baseURL: "https://play-picasso.herokuapp.com/api/",
 });
 
 export const fetchPosts = () => {
